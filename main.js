51f8494ac17ecb7139dd866da08753b718f465b3
ASGladchenko <asgladchenko@gmail.com>
2023-06-30 17:30:26 +0300

    shop_dropdown full adaptive

--- main.js
+++ main.js
@@ -4,4 +4,5 @@ const shop_dropdown = document.querySelector('.shop-dropdown');
 expend_more.addEventListener('click', () => {
   expend_more.classList.toggle('open');
   shop_dropdown.classList.toggle('open');
+  document.querySelector('body').classList.toggle('overflow-hidden');
 });

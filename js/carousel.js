70ad0a2db8c8861acbf014ce8665b59fb2198baf
Andrey Teslenko <andteslenko@gmail.com>
2023-07-01 21:53:56 +0300

    added two top and replay carousels

--- js/carousel.js
+++ js/carousel.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
-  var top = $('.top_carousel');
+  const pick = $('.pick-carousel');
+  const topLive = $('.top-live');
+  const topReplay = $('.top-replay');
 
-  top.owlCarousel({
+  pick.owlCarousel({
     margin: 30,
     nav: false,
     dots: false,
@@ -25,11 +27,75 @@ $(document).ready(function () {
     },
   });
 
-  $('.top-prev').click(function () {
-    top.trigger('prev.owl.carousel');
+  $('.pick-prev').click(function () {
+    pick.trigger('prev.owl.carousel');
   });
 
-  $('.top-next').click(function () {
-    top.trigger('next.owl.carousel');
+  $('.pick-next').click(function () {
+    pick.trigger('next.owl.carousel');
+  });
+
+  topLive.owlCarousel({
+    margin: 30,
+    nav: false,
+    dots: false,
+    loop: true,
+    navElement: 'button',
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      1200: {
+        items: 3,
+      },
+      1400: {
+        items: 4,
+        margin: 10,
+      },
+    },
+  });
+
+  $('.top-live-prev').click(function () {
+    topLive.trigger('prev.owl.carousel');
+  });
+
+  $('.top-live-next').click(function () {
+    topLive.trigger('next.owl.carousel');
+  });
+
+  topReplay.owlCarousel({
+    margin: 30,
+    nav: false,
+    dots: false,
+    loop: true,
+    navElement: 'button',
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      1200: {
+        items: 3,
+      },
+      1400: {
+        items: 4,
+        margin: 10,
+      },
+    },
+  });
+
+  $('.top-replay-prev').click(function () {
+    topReplay.trigger('prev.owl.carousel');
+  });
+
+  $('.top-replay-next').click(function () {
+    topReplay.trigger('next.owl.carousel');
   });
 });

9e05cfb17f2fb1084919eddb22d860b6f9a7b90f
ASGladchenko <105244016+ASGladchenko@users.noreply.github.com>
2023-07-02 15:29:44 +0300

    fixed (#8)

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ const shop_dropdown = document.querySelector('.shop-dropdown');
 const burger = document.querySelector('.burger');
 const header_nav = document.querySelector('.header-nav');
 
-expend_more.addEventListener('click', (event) => {
+expend_more.addEventListener('click', () => {
   if (header_nav.classList.contains('open')) {
     header_nav.classList.toggle('open');
+
+    document.querySelector('body').classList.toggle('overflow-hidden');
   }
 
   expend_more.classList.toggle('open');
@@ -20,4 +22,6 @@ burger.addEventListener('click', () => {
     document.querySelector('body').classList.toggle('overflow-hidden');
   }
   header_nav.classList.toggle('open');
+
+  document.querySelector('body').classList.toggle('overflow-hidden');
 });
